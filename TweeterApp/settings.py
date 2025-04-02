3f2c5175212bc5f7ed2c59d9ae47ff8ed087f70d
Harsh Parsaniya <154883212+HarshParsaniya@users.noreply.github.com>
2025-04-02 13:09:46 +0530

    Update settings.py

--- TweeterApp/settings.py
+++ TweeterApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e*0i+n8f7$vgk7n*4!se3eqc$!=c4&5d=p7_xrx&e2+zdlj))#'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
